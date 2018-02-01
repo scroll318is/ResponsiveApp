deac741788fbaac64efdcf942a71e2e6ad201103
Stoyan Kostov <stoyan.kostov@bulpros.com>
2018-02-01 15:38:17 +0200

    - Initial commit - Added Initial Project and .gitignore file

---
+++ ResponsiveApp/ResponsiveApp/Controller/HomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeViewController.swift
+//  ResponsiveApp
+//
+//  Created by Stoyan Kostov on 1.02.18.
+//  Copyright © 2018 Stoyan Kostov. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
