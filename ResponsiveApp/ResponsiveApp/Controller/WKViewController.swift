e6f1e1340f141db2a8a069025727526d6944beeb
Stoyan Kostov <stoyan.kostov@bulpros.com>
2018-02-04 18:05:07 +0200

    - Swipe for full screen and go back edge pan gesture recognizer added

--- ResponsiveApp/ResponsiveApp/Controller/WKViewController.swift
+++ ResponsiveApp/ResponsiveApp/Controller/WKViewController.swift
@@ -19,9 +19,15 @@ class WKViewController: UIViewController {
         super.viewDidLoad()
         setUpWebView()
         applyConstraintsToWebView()
-        let request = URLRequest(url:url)
-        webView.load(request)
-        webView.scrollView.isScrollEnabled = false
+        addPanGestureToWebView()
+        loadUrl()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if navigationController?.isNavigationBarHidden ?? false {
+            navigationController?.setNavigationBarHidden(false, animated: false)
+        }
     }
     
     // MARK: - Public Interface
@@ -32,32 +38,45 @@ class WKViewController: UIViewController {
     // MARK: - Helpers
     private func setUpWebView() {
         webView = WKWebView(frame: .zero, configuration: WKWebViewConfiguration())
-        webView.navigationDelegate = self
         webView.contentMode = .scaleToFill
+        webView.scrollView.isScrollEnabled = false
         self.view.addSubview(webView)
+    }
+    
+    private func loadUrl() {
+        let request = URLRequest(url:url)
+        webView.load(request)
+    }
+    
+    private func addPanGestureToWebView() {
+        let edgeGesture = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(onLeftHandSidePan(_:)))
+        edgeGesture.edges = .left
+        edgeGesture.delegate = self
+        webView.scrollView.addGestureRecognizer(edgeGesture)
     }
     
     private func applyConstraintsToWebView() {
         webView.translatesAutoresizingMaskIntoConstraints = false
         var constraints:[NSLayoutConstraint] = []
-        constraints.append(webView.topAnchor.constraint(equalTo: self.topLayoutGuide.bottomAnchor))
+        constraints.append(webView.topAnchor.constraint(equalTo: self.view.layoutMarginsGuide.topAnchor))
         constraints.append(webView.rightAnchor.constraint(equalTo: self.view.rightAnchor))
         constraints.append(webView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor))
         constraints.append(webView.leftAnchor.constraint(equalTo: self.view.leftAnchor))
         NSLayoutConstraint.activate(constraints)
     }
-}
-
-// MARK: - WKNavigationDelegate
-extension WKViewController: WKNavigationDelegate {
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        print(webView.url?.absoluteString as Any)
+    
+    // MARK: - Actions
+    @objc private func onLeftHandSidePan(_ sender: UIScreenEdgePanGestureRecognizer) {
+        if navigationController?.isNavigationBarHidden ?? false {
+            navigationController?.popViewController(animated: true)
+        }
     }
 }
 
-extension WKViewController: WKScriptMessageHandler {
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        print(message)
+// MARK: - UIGestureRecognizerDelegate
+extension WKViewController: UIGestureRecognizerDelegate {
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 }
 
